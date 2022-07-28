373fb6ad3909e59ec6115efa7d496fd547a98f6d
taeyanggi <taeyang.kim@gi-vita.io>
2022-07-28 22:29:11 +0900

    Changes

---
+++ src/main.rs
@@ -0,0 +1,14 @@
+fn main() {
+    // const STARTING_BAGELS: i32 = 20;
+    // const READY_TO_SERVE_BAGELS: i32 = 20;
+
+    let bagel: i32 = 10;
+    let ready: i32 = 2;
+
+    let need: i32 = bagel - ready;
+    // calc_calorie_from_elements(3.55, 3.86, 7.16);
+
+    println!("{}", need)
+}
+
+// fn calc_calorie_from_elements(flour: f32, sugar: f32, butter: f32) {}
