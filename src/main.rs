d629572ef317e8e971708a384f10e7d0243cb5eb
taeyanggi <taeyang.kim@gi-vita.io>
2022-07-30 16:25:25 +0900

    practice chapter1

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,19 @@
+const STARTING_BAGELS: i32 = 10;
+const READY_TO_SERVE_BAGELS: i32 = 2;
+
 fn main() {
-    // const STARTING_BAGELS: i32 = 20;
-    // const READY_TO_SERVE_BAGELS: i32 = 20;
+    let (mut bagel, ready) = (STARTING_BAGELS, READY_TO_SERVE_BAGELS);
 
-    let bagel: i32 = 10;
-    let ready: i32 = 2;
-
-    let need: i32 = bagel - ready;
-    // calc_calorie_from_elements(3.55, 3.86, 7.16);
-
-    println!("{}", need)
+    println!("ready to serve {} of {} bagels", ready, bagel);
+    println!("serving..!!");
+    bagel -= ready;
+    println!("now {} bagles left to serve", bagel);
+    println!(
+        "a bagel with {:.2} calories",
+        calc_calorie_from_elements(100.0, 100.0, 100.0)
+    );
 }
 
-// fn calc_calorie_from_elements(flour: f32, sugar: f32, butter: f32) {}
+fn calc_calorie_from_elements(flour: f32, sugar: f32, butter: f32) -> f32 {
+    flour * 3.55 + sugar * 3.86 + butter * 7.16
+}
